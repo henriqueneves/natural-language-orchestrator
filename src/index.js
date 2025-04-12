fedd0da90f4d9ad71bdfbd11239d69d38ef9587a
Henrique Neves <hgracioli@insider.com>
2025-04-12 11:18:43 -0300

    start langchain project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import 'dotenv/config';
+
+const model = new ChatOpenAI({ model: "gpt-4o-mini" });
+
+const messages = [
+    new SystemMessage("Translate the following from English into Italian"),
+    new HumanMessage("Hello world!"),
+  ];
+  
+const response = await model.invoke(messages);
+
+
+
+console.log(response.content);
